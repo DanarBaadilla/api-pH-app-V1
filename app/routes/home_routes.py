20e01fe879f0b9a2ee0c0d8b0fa1e54db615fa3b
Danar Rafiardi Ahmad <danarbaadilla12@gmail.com>
2024-12-20 14:02:44 +0800

    add jwt required

--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 from app.controllers.home_controller import get_banner
+from flask_jwt_extended import jwt_required
 
 home_routes = Blueprint('home_routes', __name__)
 
 @home_routes.route('/banners', methods=['GET'])
+@jwt_required()
 def banners_route():
     return get_banner()
