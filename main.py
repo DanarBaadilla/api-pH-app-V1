43254bcf92d7c8086ff2171fbfa38482b92a7549
Danar Rafiardi Ahmad <danarbaadilla12@gmail.com>
2025-01-16 14:18:49 +0800

    Register history routes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from flask_jwt_extended import JWTManager
 from app.routes.auth_routes import auth_routes
 from app.routes.home_routes import home_routes
 from app.routes.tutorial_routes import tutorial_routes
+from app.routes.history_routes import history_routes
 from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
@@ -32,6 +33,7 @@ app.register_blueprint(swagger_blueprint, url_prefix = swagger_url)
 app.register_blueprint(auth_routes)
 app.register_blueprint(home_routes)
 app.register_blueprint(tutorial_routes)
+app.register_blueprint(history_routes)
 
 if __name__ == '__main__':
     app.run(debug=True, host= "0.0.0.0", port= int(os.environ.get("PORT", 8080)))
