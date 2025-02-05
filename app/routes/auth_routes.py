d2ae5bb16ae73070db96da3d5dc44554900ae6e3
Danar Rafiardi Ahmad <danarbaadilla12@gmail.com>
2025-02-05 10:07:29 +0800

    update register routes

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -3,6 +3,6 @@ from app.controllers.auth_controller import register_guest
 
 auth_routes = Blueprint('auth_routes', __name__)
 
-@auth_routes.route('/register', methods=['POST'])
+@auth_routes.route('/registerGuest', methods=['POST'])
 def register_route():
     return register_guest()
